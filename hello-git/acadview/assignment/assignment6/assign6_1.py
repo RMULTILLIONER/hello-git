55ffe4df53354c32049fb1d6af73c450b66d2ced
RMULTILLIONER <39881254+RMULTILLIONER@users.noreply.github.com>
2018-06-12 16:23:31 +0530

    assignment6 Quest2 infinite loop

---
+++ hello-git/acadview/assignment/assignment6/assign6_1.py
@@ -0,0 +1,12 @@
+#Q2
+
+print("\n\n Q.2- Write an infinite loop.An infinite loop never ends. Condition is always true")
+output = list()
+print("\nEnter values to List : ")
+for i in range(10):
+    int_val = int(input(("Enter %d value : ") %(i+1)))  
+    output.append(int_val)
+print("Integer List : " ,output)
+while range(10):         
+ print("Infinite loop: ")
+
